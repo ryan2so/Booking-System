68ee5f077894a73e00f21c42aa7f4d6e6d25835f
RyanSo <sochun1994@hotmail.com>
2024-12-17 12:02:13 +0800

    fixed port3000 error

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import {BrowserRouter , Route , Link, Routes} from 'react-router-dom'
+import Homescreen from './screens/Homescreen';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/home" exact Component={Homescreen} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
