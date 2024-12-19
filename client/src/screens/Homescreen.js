ab6d0c15253d0358967413213e5476a5efee88bf
RyanSo <sochun1994@hotmail.com>
2024-12-19 16:27:54 +0800

    record in array but shown 0 rooms

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
 
-function Homescreen() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+const Homesceen = () => {
+ 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = (await axios.get('/api/rooms/getallrooms')).data;
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+  return (
+    <div>
+    <h1> Home Screen </h1>
+      <h1>they are {data.length} rooms</h1>
+
+    </div>
+  )
 }
-
-export default Homescreen
+export default Homesceen;
